6a4d3ab459e06437f43513fc110f03abaf646970
Dorian Stoll <dorian@tmsp.io>
2017-05-18 17:51:08 +0200

    Add the project files

---
+++ Assets/Editor/AssetExport.cs
@@ -0,0 +1,24 @@
+﻿// C# Example
+// Builds an asset bundle from the selected objects in the project view.
+// Once compiled go to "Menu" -> "Assets" and select one of the choices
+// to build the Asset Bundle
+
+using UnityEngine;
+using UnityEditor;
+
+public class ExportAssetBundles
+{
+    [MenuItem("Assets/Build Lens Flare Object")]
+    static void ExportResource()
+    {
+        // Bring up save panel
+        string path = EditorUtility.SaveFilePanel("Save Resource", "", "New Resource", "unity3d");
+        if (path.Length != 0)
+        {
+            // Build the resource file from the active selection.
+            Object[] selection = Selection.GetFiltered(typeof(Object), SelectionMode.Assets);
+            BuildPipeline.BuildAssetBundle(Selection.activeObject, selection, path, BuildAssetBundleOptions.CollectDependencies, BuildTarget.StandaloneWindows);
+            Selection.objects = selection;
+        }
+    }
+}
